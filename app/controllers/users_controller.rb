bfaa2ac29485274a7fa5b9f64f530a09dbfd90b5
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-05-12 08:36:41 +0000

    [Finish]4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+
 end
