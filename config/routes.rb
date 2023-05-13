661788999e0afe7c5d7c7fa934bb525c55ff1a8d
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-05-13 02:26:39 +0000

    [fix]post_images/index.html.erbの構文エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get 'homes/about', as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    # ↓ resourcesを単数形にすると/:idがURLに含まれなくなる
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only:[:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
